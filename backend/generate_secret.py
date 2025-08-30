47ffadaa658b90ebefa589aa1251dc57ead5f00e
rlfahs3025 <rlfahs3025@gmail.com>
2025-08-30 18:24:25 +0900

    Default passwords

--- backend/generate_secret.py
+++ backend/generate_secret.py
@@ -4,3 +4,15 @@ import base64
 secret_key = secrets.token_hex(32)
 encoded_key = base64.b64encode(secret_key.encode()).decode()
 print(encoded_key) 
+
+mariadb_raw_password = "hyunjun-mariadb"
+mariadb_encoded_password = base64.b64encode(mariadb_raw_password.encode()).decode()
+print("MYSQL_PASSWORD:", mariadb_encoded_password)
+
+redis_raw_password = "hyunjun-redis"
+redis_encoded_password = base64.b64encode(redis_raw_password.encode()).decode()
+print("REDIS_PASSWORD:", redis_encoded_password)
+
+kafka_raw_password = "hyunjun-kafka"
+kafka_encoded_password = base64.b64encode(kafka_raw_password.encode()).decode()
+print("KAFKA_PASSWORD:", kafka_encoded_password)
